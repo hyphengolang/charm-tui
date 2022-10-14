93be584a0a5bb5418773f6a4011c665943cf7676
aboublef <kristopherab@gmail.com>
2022-10-14 23:43:43 +0100

    added tabs to page

--- pages/page-01/page.go
+++ pages/page-01/page.go
@@ -4,18 +4,27 @@ import (
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/hyphengolang/charm-tui/component/tabs"
 )
 
-type Page struct{}
+type Page struct {
+	t tabs.Tabs
+}
 
-func New() Page { return Page{} }
+func New() Page { return Page{t: tabs.New()} }
 
-func (pg Page) Init() tea.Cmd { return tea.Batch() }
+func (pg Page) Init() tea.Cmd { return tea.Batch(pg.t.Init()) }
 
-func (pg Page) Update(msg tea.Msg) (tea.Model, tea.Cmd) { return pg, nil }
+func (pg Page) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	pg.t, cmd = pg.t.Update(msg)
+
+	return pg, tea.Batch(cmd)
+}
 
 func (pg Page) View() string {
 	var sb strings.Builder
 	sb.WriteString("This is Page 1!!!")
+	sb.WriteString(pg.t.View())
 	return sb.String()
 }
