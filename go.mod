f1c9719afe3f4c0f690facee4b64c35f9b3264df
aboublef <kristopherab@gmail.com>
2022-10-14 23:43:59 +0100

    update global styles

--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/exp v0.0.0-20221012211006-4de253d81b95
 	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
--- styles/styles.go
+++ styles/styles.go
@@ -2,12 +2,10 @@ package styles
 
 import (
 	css "github.com/charmbracelet/lipgloss"
-	"github.com/muesli/termenv"
 )
 
 var (
-	Color   = termenv.EnvColorProfile().Color
-	Keyword = termenv.Style{}.Foreground(Color("204")).Background(Color("235")).Styled
-	Help    = termenv.Style{}.Foreground(Color("241")).Styled
-	H1      = css.NewStyle().Foreground(css.Color("204")).Background(css.Color("235"))
+	Keyword = css.NewStyle().Foreground(css.Color("204")).Background(css.Color("235")).Render
+	Help    = css.NewStyle().Foreground(css.Color("241")).Render
+	H1      = css.NewStyle().Foreground(css.Color("204")).Background(css.Color("235")).Render
 )
